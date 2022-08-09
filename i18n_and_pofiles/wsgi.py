7d7fc44beaac5999c7eeb7eaac64257023cb747a
Felix Miño <fmino@stackbuilders.com>
2022-08-08 21:07:07 -0500

    Bootstrap i18n project

---
+++ i18n_and_pofiles/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for i18n_and_pofiles project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'i18n_and_pofiles.settings')
+
+application = get_wsgi_application()
